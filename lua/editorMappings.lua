22cd71b592aaaa799e9df6f2e9c5a71ca7bfee8e
OliverHeffernan <oliverheffernan@icloud.com>
2026-06-12 22:30:19 +1200

    Updated split view mappings to use <leader>sv and <leader>sh

--- lua/editorMappings.lua
+++ lua/editorMappings.lua
@@ -6,16 +6,15 @@ vim.keymap.set('n', '<leader>b', ':ls<CR>:b', {
 })
 
 -- mappings for splits and such
-vim.keymap.set('n', '<C-W>5', ':vertical split<CR>', {
+vim.keymap.set('n', '<leader>sv', ':vertical split<CR>', {
 	desc = "Create a vertical split.",
 	noremap = true,
 	silent = true
 })
-vim.keymap.set('n', "<C-W>'", ":split<CR>", {
+vim.keymap.set('n', "<leader>sh", ":split<CR>", {
 	desc = "Create a horizontal split.",
 	noremap = true,
 	silent = true
-
 })
 
 -- mapping to open oil by doing leader fo
@@ -56,27 +55,6 @@ vim.keymap.set('n', '<C-T>x', ':tabclose<CR>', {
 	noremap = true,
 	silent = true
 })
-
--- map jj to exit insert mode
---vim.keymap.set('i', 'jj', '<Esc>', {
-	--desc = "Escape out of insert mode into normal mode.",
-	--noremap = true,
-	--silent = true
---})
-
--- map ww to exit insert mode and write the current file
---vim.keymap.set('i', 'ww', '<Esc>:w<CR>', {
-	--desc = "Save the current file.",
-	--noremap = true,
-	--silent = true
---})
-
--- map yy to exit insert mode and yank the current line
---vim.keymap.set('i', 'yy', '<Esc>yy', {
-	--desc = "Yank the current line when in insert mode.",
-	--noremap = true,
-	--silent = true
---})
 
 vim.keymap.set('n', '<leader>d',
 	function()
